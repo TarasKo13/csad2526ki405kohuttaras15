8afaa99f5aebe52abd07cd94d19c6365b9bdcf08
Taras Kohut <tarasyura61@gmail.com>
2025-10-14 21:45:11 +0300

    Видалити папку build з репозиторію

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Taras/csad2526ki405kohuttaras15
-# Build directory: C:/Users/Taras/csad2526ki405kohuttaras15/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "C:/Users/Taras/csad2526ki405kohuttaras15/build/unit_tests.exe")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Taras/csad2526ki405kohuttaras15/CMakeLists.txt;34;add_test;C:/Users/Taras/csad2526ki405kohuttaras15/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
